721b18c147d6af67caf7aefffcdc4d60b9e735fc
Daniel D'Avella <drdavella@gmail.com>
2018-08-02 16:43:48 -0400

    First commit of new repository

---
+++ grammex.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from parsimonious.grammar import Grammar, Literal, OneOf
+
+grammar = Grammar(
+    """
+    dot = "\\."
+    sign = "[+-]?"
+    digits = "[0-9]*"
+    complex = "[iIjJ]"
+    number = digits / (dot digits) / (digits dot digits)
+    scientific = number "[eE]" sign digits
+    real = (sign number) / (sign scientific)
+    """)
+
+
+def build_regex(node):
+
+    print(node)
+    if isinstance(node, Literal):
+        return node.literal
+
+    if isinstance(node, OneOf):
+        return "|".join(["({})".format(build_regex(x)) for x in node.members])
+
+    result = ''
+    for m in node.members:
+        result += build_regex(m)
+
+    return result
+
+
+def create_regex(gram, start):
+
+    assert start in gram
+
+    return build_regex(gram[start])
